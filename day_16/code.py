2b58fabd2372c5e0133975e347ba0055ae0535bc
Alec Schule <alecschule@gmail.com>
2020-12-24 10:28:05 -0800

    day 16 finished

--- day_16/code.py
+++ day_16/code.py
@@ -14,8 +14,21 @@ scanning_error = sum(sum(int(i) for i in ticket.split(',') if not valid(int(i)))
 valid_tickets = [[int(i) for i in line.split(',')] for line in parts[2].splitlines()[1:] if all(valid(int(value)) for value in line.split(','))]
 
 field_map = {}
-for field in values.keys():
-    for i in range(len(valid_tickets[0])):
-        if all(
+for field, value in values.items():
+    field_map[field] = [i for i in range(len(valid_tickets[0])) if all(any(bottom <= ticket[i] <= top for bottom, top in value) for ticket in valid_tickets)]
 
-print(valid_tickets)
+while any(len(i) > 1 for i in field_map.values()):
+    for field, value in field_map.items():
+        if len(value) == 1:
+            for field_a in field_map.keys():
+                if field != field_a and value[0] in field_map[field_a]:
+                    field_map[field_a].remove(value[0])
+
+my_ticket = [int(i) for i in parts[1].split('\n')[1].split(',')]
+
+answer = 1
+for field in values.keys():
+    if field.startswith('departure'):
+        answer *= my_ticket[field_map[field][0]]
+
+print(answer)
